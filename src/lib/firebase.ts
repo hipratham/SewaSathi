8dfc023ff4de78772c6b07fb991f00fda8856062
Pratham <prathamthapa24@gmail.com>
2025-05-25 16:17:32 +0000

    npm install firebase Then, initialize Firebase and begin using the SDKs for the products you'd like to use.
    // Import the functions you need from the SDKs you need
    import { initializeApp } from "firebase/app";
    import { getAnalytics } from "firebase/analytics";
    // TODO: Add SDKs for Firebase products that you want to use
    // https://firebase.google.com/docs/web/setup#available-libraries
    
    // Your web app's Firebase configuration
    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    const firebaseConfig = {
      apiKey: "AIzaSyBP8Bm2gadbht0gClE6PGKjqC76JgrWY4M",
      authDomain: "sewasathi-1076b.firebaseapp.com",
      projectId: "sewasathi-1076b",
      storageBucket: "sewasathi-1076b.firebasestorage.app",
      messagingSenderId: "435729160797",
      appId: "1:435729160797:web:06be7d7d52319271048bb4",
      measurementId: "G-SEH6GJJM97"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase, type Database } from "firebase/database";
 // import { getAnalytics } from "firebase/analytics"; // Uncomment if you need analytics
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYrRsXg27Q2TWatbH05GiN5deU5BpITI",
+  apiKey: "AIzaSyBP8Bm2gadbht0gClE6PGKjqC76JgrWY4M", // Updated
   authDomain: "sewasathi-1076b.firebaseapp.com",
   projectId: "sewasathi-1076b",
   storageBucket: "sewasathi-1076b.firebasestorage.app",
   messagingSenderId: "435729160797",
-  appId: "1:435729160797:web:2d8107b3357a63c7048bb4",
-  measurementId: "G-GQDBZ1T230",
+  appId: "1:435729160797:web:06be7d7d52319271048bb4", // Updated
+  measurementId: "G-SEH6GJJM97", // Updated
   // The databaseURL is required for Realtime Database
   databaseURL: "https://sewasathi-1076b-default-rtdb.firebaseio.com"
 };
@@ -29,6 +29,6 @@ if (!getApps().length) {
 auth = getAuth(app);
 database = getDatabase(app);
 
-// const analytics = getAnalytics(app); // Uncomment if you need analytics
+// const analytics = getAnalytics(app); // Uncomment if you need analytics. Note: You had `getAnalytics` imported but not assigned if this new snippet is the sole source. Keeping it commented as per original structure.
 
 export { app, auth, database };
